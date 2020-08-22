f068593c7f3533d61b7028bce2804fa1bafa7eb9
Md Shahadat Hossain <32963275+MdShahadatHossainbd@users.noreply.github.com>
2020-08-22 15:55:19 +0600

    Add files via upload

---
+++ 905. Sort Array By Parity.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sortArrayByParity(self, A: List[int]) -> List[int]:
+        l, r = 0, len(A)-1
+        while l < r:
+            while l < r and A[l] % 2 == 0: l += 1
+            while l < r and A[r] % 2 == 1: r -= 1
+            A[l], A[r] = A[r], A[l]
+        return A
